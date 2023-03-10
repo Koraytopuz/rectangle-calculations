a682a2b300cd074549809f01a2464f3d9477ef6a
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:43:00 +0300

    Add files via upload

---
+++ fonksiyon.py
@@ -0,0 +1,16 @@
+def karesini_yaz(sayi):
+    karesi=sayi**2
+    print(karesi)
+
+karesini_yaz(5)
+
+def dikdortgen_alani_hesapla(kisa_kenar, uzun_kenar):
+    dikdortgen_alani=kisa_kenar*uzun_kenar
+    print(dikdortgen_alani)
+
+dikdortgen_alani_hesapla(5,4)
+
+def scope():
+    scope_degiskeni=100
+    print(scope_degiskeni)
+scope()
